d1f86b7e1955832990b580ad4eeb95adea139461
Adam Lucia <lucia.adam@gmail.com>
2017-02-23 20:31:23 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+  var commentTemplate =   '<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>';
+
+}
